7e5e85a362e21367f1816a915c4f291c3a39ec21
Konrad Jopek <90185027+KJopek453@users.noreply.github.com>
2021-10-19 15:39:37 +0200

    Update main.cpp

--- pracownia_programowania_obiektowego/konstruktory/main.cpp
+++ pracownia_programowania_obiektowego/konstruktory/main.cpp
@@ -6,35 +6,82 @@ struct Date
 };
 class Car
 {
-	public:
-		int id;
-		string brand, model, color;
-		unsigned short int power;
-		float price;
-		Date productionDate;
-		
-		void getData();
-		Car(int, string, string, string, unsigned short int, float, Date);
+public:
+	int id;
+	string brand, model, color;
+	unsigned short int power;
+	float price;
+	Date productionDate;
+
+	void getData();
+	Car();
+	Car(int id);
+	Car(string brand);
+	Car(int id, string brand, string model);
+	Car(int id, string brand, string model, string color);
+	Car(int id, string brand, string model, string color, unsigned short int power, float price,
+		Date productionDate);
 };
-Car::Car(int pId, string pBrand, string pModel, string pColor, unsigned short int pPower, float pPrice, Date pProductionDate)
+Car::Car()
 {
-	id=pId;
-	brand=pBrand;
-	model=pModel;
-	color=pColor;
-	power=pPower;
-	price=pPrice;
-	productionDate=pProductionDate;
+
+}
+Car::Car(string pBrand) 
+{
+	brand = pBrand;
+}
+Car::Car(int pId)
+{
+	id = pId;
+}
+Car::Car(int pId, string pBrand, string pModel)
+{
+	id = pId;
+	brand = pBrand;
+	model = pModel;
+}
+Car::Car(int pId, string pBrand, string pModel, string pColor)
+{
+	id = pId;
+	brand = pBrand;
+	model = pModel;
+	color = pColor;
+}
+Car::Car(int pId, string pBrand, string pModel, string pColor, unsigned short int pPower, float pPrice,
+	Date pProductionDate)
+{
+	id = pId;
+	brand = pBrand;
+	model = pModel;
+	color = pColor;
+	power = pPower;
+	price = pPrice;
+	productionDate = pProductionDate;
 }
 void Car::getData()
 {
-	cout<<"\nId: "<<id<<"\nMarka: "<<brand<<"\nModel: "<<model<<"\nKolor: "<<color
-	<<"\nMoc: "<<power<<"km"<<"\nCena: "<<price<<"z³"<<"\nRok produkcji: "<<productionDate.yyyy<<"r.";
+	cout << "\nId: " << id << "\nMarka: " << brand << "\nModel: " << model << "\nKolor: " << color
+		<< "\nMoc: " << power << "km" << "\nCena: " << price << "zÂ³" << "\nRok produkcji: "
+		<< productionDate.yyyy << "r.\n";
 }
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
-	setlocale(LC_CTYPE,"polish");
-	Car fiat(1, "x", "y", "z", 1, 1, {1,1,1});
+	setlocale(LC_CTYPE, "polish");
+	Car fiat(8, "Fiat", "126p", "czerwony", 38, 20000, {19, 10, 2021});
 	fiat.getData();
+
+	Car honda;
+	honda.getData();
+
+	Car toyota(13, "Toyota", "Yaris");
+	toyota.getData();
+
+	Car toyota1(13, "Toyota", "Yaris", "Czerwony");
+	toyota1.getData();
+
+	Car *p_zsk;
+	p_zsk = &fiat;
+	cout<<"\nMarka: "<<p_zsk->brand;
+
 	return 0;
 }
