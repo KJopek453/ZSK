4f11841a1518db3c9cfcdce92411357845f32323
Konrad Jopek <90185027+KJopek453@users.noreply.github.com>
2021-10-26 15:44:56 +0200

    Add files via upload

---
+++ pracownia_programowania_obiektowego/konstruktor_kopiujący/konstruktor_kopiujący.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+private:
+
+    double sideA;
+    double sideB;
+
+public:
+
+    Rectangle();
+    Retangle(double sideA, double sideB);
+//prototyp konstruktora kopiującego
+    Rectangle(const Rectangle&);
+
+};
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+
+
+
+    return 0;
+}
---
+++ pracownia_programowania_obiektowego/listy_inicjalizacyjne/inicjalizacja_obiektu.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Worker
+{
+	const int id{ 0 };
+	string name, surname;
+
+public:
+	
+	Worker();
+	Worker(int id, string name, string surname);
+	void getData();
+
+};
+
+Worker::Worker():
+	id{ -1 },
+	name{ "Imię domyślne" },
+	surname{ "Nazwisko domyślne" }
+{
+	cout <<endl<< "Konstruktor domyślny"<<endl;
+}
+
+Worker::Worker(int pId, string pName, string pSurname):
+	id{pId},
+	name{ pName },
+	surname{ pSurname }
+{
+	cout <<endl<< "Konstruktor parametryczny"<<endl;
+}
+
+void Worker::getData()
+{
+	cout << endl << "imię: " << name << endl << "Nazwisko: " << surname << endl << "Id: " << id<<endl;
+}
+
+int main(int argc, char** argv) {
+	setlocale(LC_CTYPE, "polish");
+
+	Worker nowak;
+	nowak.getData();
+
+	Worker nowak2=Worker();
+	nowak2.getData();
+
+	Worker kowalski(10,"Janusz","Kowalski");
+	kowalski.getData();
+
+	Worker kowalski2=Worker(10, "Janusz2", "Kowalski2");
+	kowalski2.getData();
+
+	return 0;
+}
