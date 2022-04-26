8fed246825471ff23a9af2a6ae3effe2754e404f
Konrad Jopek <90185027+KJopek453@users.noreply.github.com>
2022-04-26 15:43:10 +0200

    Add files via upload

--- pracownia_programowania_obiektowego/pliki/main.cpp
+++ pracownia_programowania_obiektowego/pliki/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 
 using std::cout;
 using std::string;
@@ -8,13 +9,14 @@ using std::cin;
 
 int main(int argc, char** argv) {
 	setlocale(LC_ALL, "polish");
-	
+
+
 	std::ofstream File;
-	File.open("C:\\Users\\student\\Desktop\\pliki2\\plik.txt", std::ios::app);
-	
+	File.open("plik.txt", std::ios::app);
+
 	if(File.is_open())
 	{
-		cout<<"Podaj dane do zapisu w pliku"<<endl;
+		cout << "Podaj dane do zapisu w pliku" << endl;
 		string text;
 		getline(cin,text);
 		File << text << endl;
@@ -24,7 +26,18 @@ int main(int argc, char** argv) {
 	{
 		cout << "error";
 	}
-	
-	
+
+	system("cls");
+
+	std::ifstream FileOpen("plik.txt");
+	if(FileOpen.is_open()){
+        cout << "Zawartoœæ pliku: " << endl;
+        string line;
+        while(getline(FileOpen, line)) cout << line << endl;
+        FileOpen.close();
+	}
+	else{
+        cout << "error";
+	}
 	return 0;
 }
